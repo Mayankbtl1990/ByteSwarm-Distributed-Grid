a533e2b67265da19a274f17f347ef35660c65188
bhanugunupati <bhanugunupati@gmail.com>
2026-08-10 02:33:56 +0530

    maven project with netty dependencies

--- backend/src/main/java/com/byteswarm/BackendApplication.java
+++ backend/src/main/java/com/byteswarm/BackendApplication.java
@@ -1,13 +1,13 @@
 package com.byteswarm;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		//SpringApplication.run(BackendApplication.class, args);
+		System.out.println("╔══════════════════════════════════════╗");
+        System.out.println("║   🐝 ByteSwarm Backend Initialized   ║");
+        System.out.println("╚══════════════════════════════════════╝");
 	}
 
 }
